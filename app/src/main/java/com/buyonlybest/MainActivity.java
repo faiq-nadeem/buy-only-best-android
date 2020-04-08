427e5bbd0766a1130ab7c7707974fe20ee006b1e
faiq nadeem <chfaiqnadeem@gmail.com>
2020-04-08 15:03:40 +0500

    Working app with full page for mobile features working no issue, added on back press action menu working.

--- app/src/main/java/com/buyonlybest/MainActivity.java
+++ app/src/main/java/com/buyonlybest/MainActivity.java
@@ -2,6 +2,8 @@ package com.buyonlybest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,8 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("BUY ONLY BEST ITEM");
 
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.MyAlertDialog);
+        builder.setTitle("ARE YOU SURE, YOU WANT TO EXIT?");
+        builder.setMessage("If you need any please Contact Us!");
+        builder.setNegativeButton("NEED HELP", null);
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finishAffinity();
+            }
+
+        }).show();
+
+        
     }
 
     public void mobile_tablet(View view){
